874eb6d067d7be50187e4a350e874d121844ee4b
Forth <33458877+Forthtilliath@users.noreply.github.com>
2023-05-15 04:46:49 +0200

    feat: sidebar

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { For } from "solid-js";
 import { Card } from "~/components/Card";
+import { Sidebar } from "~/components/Sidebar";
 import { codepens } from "~/utils/data";
 
 export default function Home() {
@@ -8,10 +9,13 @@ export default function Home() {
       <h1 class="max-6-xs text-6xl text-sky-700 dark:text-sky-300 font-bold uppercase my-16">
         Bienvenue sur la HomePage de mon Github
       </h1>
+
+      <Sidebar />
+
       <div class="container my-12 mx-auto px-4 md:px-12">
         <div class="flex flex-wrap -mx-1 lg:-mx-4">
           <For each={codepens} fallback="Chargement...">
-            {(codepen => <Card {...codepen} />)}
+            {(codepen) => <Card {...codepen} />}
           </For>
         </div>
       </div>
