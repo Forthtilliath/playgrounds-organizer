d864d27d4fe659cabcb19aa9c2fcc2622d5c7153
Forthtilliath <forth.developer@gmail.com>
2023-05-31 00:01:42 +0200

    build: add netlify config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
+import netlify from "solid-start-netlify";
 
 export default defineConfig({
-  plugins: [solid({ ssr: false })],
+  plugins: [
+    solid({
+      ssr: false,
+      adapter: netlify({ edge: true }),
+    }),
+  ],
 });
