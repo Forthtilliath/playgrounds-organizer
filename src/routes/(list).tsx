20dcf8be8b6852a12ec52b7b6e21eea20b6f3a59
Forthtilliath <forth@live.fr>
2023-11-29 13:38:01 +0100

    feat: change title + move container class

--- src/routes/(list).tsx
+++ src/routes/(list).tsx
@@ -5,14 +5,14 @@ import { codepens } from "~/utils/data";
 
 export default function Home() {
   return (
-    <main class="text-center mx-auto text-gray-700 p-4">
+    <main class="container text-center mx-auto text-gray-700 p-4">
       <h1 class="max-6-xs text-6xl text-sky-700 dark:text-sky-300 font-bold uppercase my-16">
-        Bienvenue sur la HomePage de mon Codepen
+        Bienvenue sur mon organiseur de Playgrounds
       </h1>
 
       <Sidebar />
 
-      <div class="container my-12 mx-auto px-4 md:px-12">
+      <div class="my-12 mx-auto px-4 md:px-12">
         <div class="flex flex-wrap -mx-1 lg:-mx-4">
           <For each={codepens} fallback="Chargement...">
             {(codepen) => <Card {...codepen} />}
