8b72f8f5be4d91cae8f21af3141b260b198f473e
Forthtilliath <forth@live.fr>
2023-11-29 13:37:05 +0100

    feat: add boxShadow

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      boxShadow: {
+        card: "-2px -2px 5px hsl(222 47% 16% / 1), 2px 2px 5px hsl(222 47% 21% / 1)",
+        "card-xl": "-5px -5px 15px hsl(222 47% 16% / 1), 5px 5px 15px hsl(222 47% 21% / 1)",
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
