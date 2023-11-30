42cd60c1b68d38740d2d68727bf79c704948eda3
Forthtilliath <forth@live.fr>
2023-11-30 01:28:54 +0100

    feat: rename file & add toggleTag

--- src/lib/stores/filterStore.ts
+++ src/lib/stores/filterStore.ts
@@ -1,21 +1,30 @@
 import { createStore } from "solid-js/store";
 
-export const [filter, setFilter] = createStore<FilterStore>({
+const [filter, setFilter] = createStore<FilterStore>({
   query: "",
   tags: [],
   type: "conjunction",
 
-  addTagToFilter(tag: Tag) {
+  addTag(tag: Tag) {
     setFilter("tags", (tags) => [...tags, tag]);
-    // setFilter(produce((state) => state.tags.push(tag)));
   },
-  removeTagToFilter(tag: Tag) {
+  removeTag(tag: Tag) {
     setFilter("tags", (tags) => tags.filter((t) => t !== tag));
-    // setFilter(produce((state) => state.tags.filter((t) => t !== tag)));
+  },
+  toggleTag(tag: Tag) {
+    setFilter("tags", (tags) => {
+      if (tags.includes(tag)) {
+        return tags.filter((t) => t !== tag);
+      } else {
+        return [...tags, tag];
+      }
+    });
   },
   setType(type: FilterStore["type"]) {
     setFilter("type", type);
   },
 });
+
+export const filterStore = filter;
 
 // https://www.solidjs.com/docs/latest#reconcile
